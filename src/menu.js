1c48acf72160ccac3b00e80a9832a1d15d758598
enoch harnanto <enochharnantoo@gmail.com>
2022-03-26 00:33:50 -0700

    Added createMenuEntry function

--- src/menu.js
+++ src/menu.js
@@ -1,19 +1,44 @@
-function createHeader() {
-    const header = document.createElement('div'); 
-    header.innerText = "Menu";
-}
-
 function createMenu() {
     const menu = document.createElement('div');
-    menu.innerText = "Menu";
+    menu.classList.add('menu');
+    menu.appendChild(
+        createMenuEntry(
+            'Filet Mignon' ,
+            'Juicy and delicious', 
+            '$45'
+        )
+    );
     return menu;
+}
+
+function createMenuEntry(name, description, price) {
+    const menuEntry = document.createElement('div'); 
+    menuEntry.classList.add('menu-entry');
+
+    const title = document.createElement('h2');
+    title.textContent = name;
+
+    const describe = document.createElement('p');
+    describe.textContent = description;
+
+    const cost = document.createElement('p');
+    cost.textContent = price;
+
+    // const foodPic = document.createElement('img');
+    // foodPic.src = PLACEHOLDER;
+    // foodPic.alt = PLACEHOLDER;
+
+    menuEntry.appendChild(title);
+    menuEntry.appendChild(describe);
+    menuEntry.appendChild(cost); 
+
+    return menuEntry;
 }
 
 function loadMenu() {
     const main = document.getElementById('main');
+    main.textContent = "";
     main.appendChild(createMenu());
 }
-
-
 
 export default loadMenu; 
